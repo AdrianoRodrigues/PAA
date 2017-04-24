f2f1dbaa818c4ff1c0b9cf3a2cbd2e995bb64fc5
Adriano Silva <frs.adriano@gmail.com>
2017-04-24 06:33:59 -0300

    Adicionado solucao do problema queue.

--- Exercicio_D.1/p10128.py
+++ Exercicio_D.1/p10128.py
@@ -1,3 +1,14 @@
+import itertools
+
+a = range(1, 5)
+
+solutions = list(itertools.permutations(range(13)))
+
+counter = 1
+for sol in solutions:
+    print(str(counter) + ': ' + str(sol))
+    counter+=1
+
 def permutation(n, p, r, index):
     if index == n:
         print('terminou')
